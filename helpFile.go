1587d11fbdae8744391ddc8bc7860b345f6a7109
7RU7H <100765038+7RU7H@users.noreply.github.com>
2022-04-14 18:20:41 +0100

    Create helpFile.go

---
+++ helpFile.go
@@ -0,0 +1,12 @@
+func checkExists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		log.Fatal(err)
+		return false
+	}
+	if os.IsNotExist(err) {
+		log.Fatal("Not Exists")
+		return false
+	}
+	return true
+}
