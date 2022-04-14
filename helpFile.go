5b518cf2677fbb23e7a63c0737f64ac28021aade
7RU7H <100765038+7RU7H@users.noreply.github.com>
2022-04-14 18:26:33 +0100

    Update helpFile.go

--- helpFile.go
+++ helpFile.go
@@ -5,8 +5,18 @@ func checkExists(path string) bool {
 		return false
 	}
 	if os.IsNotExist(err) {
-		log.Fatal("Not Exists")
+		log.Fatal("Path does not Exists")
 		return false
 	}
 	return true
+}
+
+
+func isDirectory(path string) (bool, error) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+
+	return fileInfo.IsDir(), err
 }
