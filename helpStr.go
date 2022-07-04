a88b7ad09229aa700d1ad0b94650e70541bbb964
mkthun <mkthun@protonmail.com>
2022-07-04 19:18:27 +0100

    Scrapped gufferGopherflow for new IPv6 enum tool

--- helpStr.go
+++ helpStr.go
@@ -24,3 +24,35 @@ func replaceWS20(scrap, collect string, size int) string {
   result := strings.Join(data, "")
 	return result
 }
+
+func reverseStrEndianness(s string) string {
+	builder := strings.Builder{}
+	var editStr string
+	if strings.Contains(s, "0x") {
+		editStr = strings.TrimPrefix(s, "0x")
+	} else {
+		editStr = s
+	}
+	sizeS := len(editStr) - 1
+	pieces, piecesSize := explodeToPairs(editStr, sizeS)
+	for i := piecesSize - 1; i >= 0; i-- {
+		builder.WriteString(pieces[i])
+	}
+	return builder.String()
+}
+
+
+func covertStrToByte(s string) []byte {
+  result := byte[](s)
+	return result
+}
+
+func add0xStub(s string) string {
+	addzeroX := "0x" + s
+	return addzeroX
+}
+
+func remove0xStub(s string) string {
+	stubless := strings.TrimPrefix(s, "0x")
+	return stubless
+}
