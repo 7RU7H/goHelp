a75afa500a94c76d741712306b890b25f230f727
truth <mkthun@protonmail.com>
2023-07-18 22:40:19 +0100

    Fixes spelling, naming and added error output

--- helpFile.go
+++ helpFile.go
@@ -1,22 +1,22 @@
-func checkExists(path string) bool {
-	_, err := os.Stat(path)
-	if err == nil {
-		log.Fatal(err)
-		return false
-	}
-	if os.IsNotExist(err) {
-		log.Fatal("Path does not Exists")
-		return false
-	}
-	return true
+func checkFileExists(path string) (bool error) {
+        _, err := os.Stat(path)
+        if err == nil {
+                log.Fatal(err)
+                return false, err
+        }
+        if os.IsNotExist(err) {
+                log.Fatal("File path does not exist")
+                return false, err
+        }
+        return true, nil
 }
 
 
 func isDirectory(path string) (bool, error) {
-	fileInfo, err := os.Stat(path)
-	if err != nil {
-		return false, err
-	}
+        fileInfo, err := os.Stat(path)
+        if err != nil {
+                return false, err
+        }
 
-	return fileInfo.IsDir(), err
+        return fileInfo.IsDir(), err
 }
