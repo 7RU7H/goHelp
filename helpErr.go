6b5f4ec62e27f9396ccba090e01e996b58302692
7RU7H <100765038+7RU7H@users.noreply.github.com>
2025-08-14 21:12:10 +0100

    Update helpErr.go
    Improved error handling with int for id and allowing for unpanicing, but requires logging

--- helpErr.go
+++ helpErr.go
@@ -1,5 +1,33 @@
-func checkError(err error) {
-        if err != nil {
-                log.Fatal(err)
-        }
+import (
+	"log"
+	"runtime"
+)
+
+func ErrorHandling(err error, level int) error {
+	switch level {
+	case 0:
+		log.Printf("Non fatal error: %v \n", err)
+		break
+	case 1:
+		defer panicRecovery(&err)
+		return err
+	case 2:
+		log.Fatalln(err)
+		panic(err)
+	}
+
+	return nil
+}
+
+func panicRecovery(err *error) error {
+	if r := recover(); r != nil {
+		if _, ok := r.(runtime.Error); ok {
+			log.Println("Panicing!!")
+			panic(r)
+		} else {
+			*err = r.(error)
+			return nil
+		}
+	}
+	return nil
 }
